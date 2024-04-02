afd5590d322f683af18fa08de3934ab13eee1d9b
cjj <2557036750@qq.com>
2024-04-02 21:37:07 +0800

    [ADD]dev demo4

--- src/main/java/com/cjj/master/Demo1.java
+++ src/main/java/com/cjj/master/Demo1.java
@@ -4,5 +4,6 @@ public class Demo1 {
     public static void main(String[] args) {
         System.out.println("master demo1");
         System.out.println("master dev demo1");
+        System.out.println("修改了的");
     }
 }
