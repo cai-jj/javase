289b8dce8d6c1b23f1d06a6bf4a8c4f66ecc22c6
cjj <2557036750@qq.com>
2024-04-02 22:15:24 +0800

    [ADD]common类的A、B方法

--- src/main/java/com/cjj/master/Common.java
+++ src/main/java/com/cjj/master/Common.java
@@ -2,5 +2,15 @@ package com.cjj.master;
 
 public class Common {
 
+    public void A() {
+        System.out.println("dev1 A");
+        System.out.println("A ..........");
+    }
+
+    public void B() {
+        System.out.println("dev1 B");
+        System.out.println("B ..........");
+    }
+
 
 }
