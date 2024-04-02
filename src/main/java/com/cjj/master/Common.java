017e8bc3fcf8d5d99a69e2091deb2dfa8ad3ccc8
cjj <2557036750@qq.com>
2024-04-02 22:23:19 +0800

    [ADD]A

--- src/main/java/com/cjj/master/Common.java
+++ src/main/java/com/cjj/master/Common.java
@@ -5,6 +5,7 @@ public class Common {
     public void A() {
         System.out.println("dev1 A");
         System.out.println("A ..........");
+        System.out.println("添加A");
     }
 
     public void B() {
